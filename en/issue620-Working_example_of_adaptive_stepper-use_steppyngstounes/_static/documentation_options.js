4fbcc3defccb349269ee1b1e59bafae82d0183fc
GitHub Action <action@github.com>
2025-06-05 19:24:07 +0000

    Update documentation for issue620-Working_example_of_adaptive_stepper-use_steppyngstounes@50d8f49

--- en/issue620-Working_example_of_adaptive_stepper-use_steppyngstounes/_static/documentation_options.js
+++ en/issue620-Working_example_of_adaptive_stepper-use_steppyngstounes/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '3.1.3+893.g4999a7706',
+    VERSION: '3.1.3+894.g50d8f4938',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
